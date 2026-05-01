a59da14cbebb280b98074d4baf19051b331b5539
PRAVESH DHAKAL <pravdhakal110@gmail.com>
2026-05-01 19:38:03 +0530

    Remove node_modules and update gitignore

--- backend-todo/models/model.todo.ts
+++ backend-todo/models/model.todo.ts
@@ -1,14 +1,15 @@
-import mongoose,{Schema} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import { ITodo } from '../types/type.todo';
 
 const todoSchema = new Schema<ITodo>({
-    text:{
-        type:String,
+    text: {
+        type: String,
         required: true
     },
-    completed:{
-        type:Boolean,
-        required: true
+    completed: {
+        type: Boolean,
+        required: true,
+        enum: ['Yes', 'No']
     }
 })
-export const Todo= mongoose.model<ITodo>('Todo',todoSchema);
+export const Todo = mongoose.model<ITodo>('Todo', todoSchema);
--- backend-todo/server.ts
+++ backend-todo/server.ts
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 import app from './app'
+require('dotenv').config();
 
 const PORT= 5000;
-const MONGO_URI = 'your_mongodb_connection_string';
 
 const startServer= async()=>{
     try{
-        await mongoose.connect(MONGO_URI);
+        if (!process.env.MONGODB_URI) {
+            throw new Error("MONGODB_URI is not defined in the environment variables");
+        }
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected");
         app.listen(PORT,()=>{
             console.log("Listening on port 5000")
